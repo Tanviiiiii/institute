a0e2d9242b7a68e35f45239d14d9d7517daf92f3
Faraz-mobin17 <faraz.mobin17@gmail.com>
2021-08-27 22:46:21 +0530

    Changed the structure of the files and folders and removed register_a and loginprocess file

--- s2c_website/logout.php
+++ s2c_website/logout.php
@@ -1,6 +1,5 @@
-<?php  
+<?php
 session_start(); //session is a way to store information (in variables) to be used across multiple pages.  
 session_unset();
-session_destroy();  
-header("Location:../s2c_website\html\index.html"); //use for the redirection to some page  
-?>  
+session_destroy();
+header("Location: index.html "); //use for the redirection to some page  
