4351d23593a55c7f62b7ac727cc8ebef94619c6e
Tanviiiiii <tanvisingla3781@gmail.com>
2021-09-07 13:58:20 +0530

    student management updated

--- includes/dashboardHeader.php
+++ includes/dashboardHeader.php
@@ -1,9 +1,8 @@
-<div class="header bg-primary" style="height:200px;">
+<div class="header bg-primary" style="height:150px;">
     <div class="container">
         <div class="header-body">
             <div class="row align-items-center py-4">
                 <div class="col-lg-6 col-7">
-
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                             <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i></a></li>
