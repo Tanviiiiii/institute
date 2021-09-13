3b7caa71b077629e74fd15ec3dfbb6c5e5079abd
Faraz-mobin17 <faraz.mobin17@gmail.com>
2021-09-13 13:39:34 +0530

    Added db file

---
+++ sql_files/institute (1).sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2021 at 10:04 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `institute`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment`
+--
+
+CREATE TABLE `assignment` (
+  `id` int(11) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `concat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classlink`
+--
+
+CREATE TABLE `classlink` (
+  `id` int(11) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crud`
+--
+
+CREATE TABLE `crud` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `crud`
+--
+
+INSERT INTO `crud` (`id`, `name`, `email`, `phone`, `city`) VALUES
+(40, 'divya', 'amohapatra7000@gmail.com', '9114950911', 'balasore'),
+(42, 'Divyasundar sahu', 'amohapatra7000@gmail.com', '999999999', 'balasore');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `subject` varchar(1000) NOT NULL,
+  `class` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `firstname`, `lastname`, `email`, `password`, `phone`, `gender`, `subject`, `class`) VALUES
+(7, 'manan', 'sharma', 'manan.sharma@gmail.com', 'asdf', '7418529630', 'm', 'Biology', '12'),
+(8, 'admin', 'admin', 'admin@something.com', 'admin', '9632145698', 'M', 'N/A', 'N/A'),
+(9, 'faraz', 'mobeen', 'funnypicture23@gmail.com', 'asdf', '9633698521', 'm', 'Mathematics', '12'),
+(10, 'ravi', 'chant', 'ravi.chant@gmail.com', 'asdf', '7411477410', 'm', 'Physics, Chemistry, Mathematics', '12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studymaterial`
+--
+
+CREATE TABLE `studymaterial` (
+  `id` int(11) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `concat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `id` int(11) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `timings` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `timetable`
+--
+
+INSERT INTO `timetable` (`id`, `class`, `subject`, `day`, `timings`) VALUES
+(3, '12', 'MATHEMATICS', 'Monday', '11-12'),
+(5, '12', 'ENGLISH', 'Monday', '13-15'),
+(6, '12', 'ENGLISH', 'Monday', '13-14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assignment`
+--
+ALTER TABLE `assignment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `classlink`
+--
+ALTER TABLE `classlink`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `crud`
+--
+ALTER TABLE `crud`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `studymaterial`
+--
+ALTER TABLE `studymaterial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `timetable`
+--
+ALTER TABLE `timetable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assignment`
+--
+ALTER TABLE `assignment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `classlink`
+--
+ALTER TABLE `classlink`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `crud`
+--
+ALTER TABLE `crud`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `studymaterial`
+--
+ALTER TABLE `studymaterial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `timetable`
+--
+ALTER TABLE `timetable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
